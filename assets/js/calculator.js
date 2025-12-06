94c051b17467390c842dd2ef7e515ef1536fea8e
AJK888 <akostas12344@gmail.com>
2025-12-05 19:33:57 -0500

    1

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -445,7 +445,7 @@ const AstrologyCalculator = {
 	displayInitialResults(chartData) {
         // Display snapshot reading if available
 		if (chartData.snapshot_reading && chartData.snapshot_reading !== null) {
-			this.snapshotTitle.parentElement.style.display = 'block';
+			this.snapshotTitle.style.display = 'block';
 			this.snapshotOutput.innerHTML = chartData.snapshot_reading.replace(/\n/g, '<br>');
 		}
 		
